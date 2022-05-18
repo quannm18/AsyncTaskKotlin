48e151849d3359ba0738eae0881d3ec1f5057eff
Ngo Minh Quan <ngominhquan20021809@gmail.com>
2022-05-18 21:48:02 +0700

    Quan

---
+++ app/src/main/java/com/quannm18/asynctask/DemoGithubActivity.kt
@@ -0,0 +1,11 @@
+package com.quannm18.asynctask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoGithubActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_github2)
+    }
+}
